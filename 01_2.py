66923e94b09881231689c28dae6a0995ca04ec67
Syedumar78 <syedumariconic@gmail.com>
2024-09-26 16:30:18 +0530

    second code

---
+++ 01_2.py
@@ -0,0 +1,9 @@
+calc_to_units = 24*60*60
+name_of_unit = "seconds"
+print(f"20 days are {20*24*60} mintues and 20 days are {20*24*60*60}  Seconds")
+
+
+
+calc_to_units = 24*60*60
+name_of_unit = "seconds"
+print(f"20 days are { 20*calc_to_units} {name_of_unit}")
