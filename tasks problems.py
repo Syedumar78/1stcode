69f3f0f63868bfcc12c0f0e85f66a7e19b47b076
Syedumar78 <syedumariconic@gmail.com>
2024-09-26 16:11:10 +0530

    firstcode

---
+++ tasks problems.py
@@ -0,0 +1,38 @@
+Syed = 'umar'
+print(type(Syed))
+#-------------------#
+syed_umar =("   'average'")
+cleaned_sting = syed_umar.strip()
+print(cleaned_sting)
+#--------------------------#
+Umar = 890
+print(type(Umar))
+#---------------------------------#
+_syed = 900.90
+print(type(_syed))
+#----------------------------------#
+_umar = "    ('syed, umar', 'NAWAZ')  "
+upper_case_string = _umar.upper()
+print(upper_case_string)
+lower_case_string = _umar.lower()
+print(lower_case_string)
+cleaned_string = _umar.strip()
+print(cleaned_string)
+#------------------------------#
+_mar = "('syed, umar', 'python')  "
+new_mar_string = _mar.replace('python', "NAWAZ")
+print(new_mar_string)
+#---------------------------------#
+my_tuple = (1, "hello", 3.14)
+print(my_tuple[1])  # Output: "hello"
+#-------------------------------------#
+my_list = [189, 59999, 499999093, 5000000,]
+print(my_list)
+print(my_list[0])
+print(f"in my list the 1st number is {my_list[0]} and the last number is {my_list[3]}")
+#----------------------------------------#
+_My_tuple = (1000, 2000, 3000, 5000,)
+print(_My_tuple)
+print(_My_tuple[2])
+print(f"my first number is {_My_tuple[1]} and the last one is {_My_tuple[3]}")
+#-----------------------------------------#
